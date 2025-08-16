e69258d787642c7cd9962c28abbc30fc872a21ec
ben199712 <orijibenjamin1997@gmail.com>
2025-08-16 20:56:46 +0100

    requirment.txt added

--- build.sh
+++ build.sh
@@ -2,14 +2,6 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip first
-pip install --upgrade pip
-
-# Install packages with better error handling
-pip install -r requirements.txt --no-cache-dir
-
-# Collect static files
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate 
