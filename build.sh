697a4dce6b969e8db42abc884d6f72586a8d52a8
ben199712 <orijibenjamin1997@gmail.com>
2025-08-16 21:04:38 +0100

    the requirment.txt file

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Upgrade pip first
+pip install --upgrade pip
+
+# Install packages with optimizations for free plan
+pip install -r requirements.txt --no-cache-dir --prefer-binary
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate 
